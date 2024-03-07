57746495ee01993b523ee36997801c2412652280
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-07 11:40:00 +0100

    server moment task handled

--- server_1/server.js
+++ server_1/server.js
@@ -3,6 +3,21 @@
 const path = require("path")
 const express = require("express")
 
+const moment = require("moment");
+require("moment-timezone");
+
+ const generateTimezoneHTml = (timeInStockholm, timeInSeoul) => `
+        <html>
+            <body>
+                <p>Stockholm: ${timeInStockholm.format(
+                  "YYYY-MM-DD HH:mm:ss"
+                )}</p>
+                <p>Seoul: ${timeInSeoul.format("YYYY-MM-DD HH:mm:ss")}</p>
+            </body>
+        </html>
+
+    `;
+
 const distPath = path.join(__dirname, "/dist")
 const staticPath = path.join(distPath, "/dist");
 
@@ -27,6 +42,37 @@ app.get("/contact", (req, res) => {
 app.post("/contact", (req, res) => {
     console.log(req.body)
     res.sendFile(`${distPath}/response.html`);
+})
+
+// GET: /now
+app.get("/now", (req, res) => {
+    const timeInStockholm = moment().tz("Europe/Stockholm");
+    const timeInSeoul = moment().tz("Asia/Seoul");
+
+    console.log(timeInStockholm)
+    console.log(timeInSeoul);
+
+   
+    res.send(generateTimezoneHTml(timeInStockholm, timeInSeoul));
+})
+
+app.get("/now/form", (req, res) => {
+    res.sendFile(`${distPath}/now_form.html`)
+})
+
+app.post("/now/form", (req, res) => {
+    const { datetime } = req.body
+
+    const timeInStockholm = moment(datetime).tz("Europe/Stockholm")
+    const formattedTime = timeInStockholm.format("YYYY-MM-DD HH:mm")
+    console.log(formattedTime);
+
+    const timeInSeoul = timeInStockholm.clone().tz("Asia/Seoul")
+    const formattedTimeInSeoul = timeInSeoul.format("YYYY-MM-DD HH:mm");
+    console.log(formattedTimeInSeoul);
+
+    res.send(generateTimezoneHTml(timeInStockholm, timeInSeoul));
+
 })
 
 
