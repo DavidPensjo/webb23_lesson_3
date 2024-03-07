158e03bb90af0f83c265376d53277f9351162198
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-07 13:34:13 +0100

    get requests added

--- api/server.js
+++ api/server.js
@@ -8,14 +8,30 @@ const app = express();
 const users = [
   { id: 1, name: "Carl" },
   { id: 2, name: "Markus" },
-  { id: 2, name: "Tilda" },
+  { id: 3, name: "Tilda" },
 ];
 
 // Use middleware to handle url encoded data
 app.use(express.urlencoded({ extended: true }))
 
+app.use(express.json())
+
+
+// GET: users - get all users
 app.get("/api/v1/users", (req, res) => {
     res.json(users)
+})
+
+// GET: user - get spesific user on id
+app.get("/api/v1/users/:id", (req, res) => {
+    const id = req.params.id
+    const user = users.find(u => u.id == id)
+    if (!user) {
+        res.status(404).json({
+            message: "User not found"
+        })
+    }
+    res.json(user)
 })
 
 app.listen(3000, () => {
