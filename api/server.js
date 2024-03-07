1e93ead80387610474a187eb7cf534c4c987eeb6
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-07 14:47:18 +0100

    add update reques

--- api/server.js
+++ api/server.js
@@ -49,20 +49,63 @@ app.post("/api/v1/users", (req, res) => {
     res.json(users)
 })
 
-
 // GET: user - get spesific user on id
 app.get("/api/v1/users/:id", (req, res) => {
     const id = req.params.id
     const user = users.find(u => u.id == id)
     if (!user) {
-        res.status(404).json({
+        return res.status(404).json({
             message: "User not found"
         })
     }
     res.json(user)
 })
 
+// PUT user - updates spesific user on id
+app.put("/api/v1/users/:id", (req, res) => {
+    const id = req.params.id;
+    const userIndex = users.findIndex((u) => u.id == id);
+    const user = users?.[userIndex]
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const newUser = req.body
+    if (user.id != newUser.id){
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    if (!newUser.name) {
+        return res.status(400).json({
+          message: "Must have a valid name",
+        });
+    }
+
+    users[userIndex] = {
+        ...users[userIndex],
+        ...newUser
+    }
+
+    res.json(users)
+})
+
+
+
 
 app.listen(3000, () => {
     console.log("Server listening on :3000")
 })
+
+
+// 200 - Ok
+// 201 - Created
+// 400 - Bad request
+//  for example missing name
+// 401 - Unauthorized
+// 403 - Forbidden
+// 404 - Not found
+// 500 - Internal server error
